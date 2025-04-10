de44b965f031251f1e8ec739e6f01c124f34ca41
Arnaud301 <arnaud2bonnet@gmail.com>
2025-04-11 01:07:30 +0200

    0th task

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,7 +3,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node_n = malloc(sizeof(binary_tree_t));
 
-	if (new_n == NULL)
+	if (node_n == NULL)
 		return (NULL);
 
 	node_n->n = value;
