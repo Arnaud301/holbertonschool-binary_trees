80ebde34e8150c499ebcb0e2bf8665fb0708b015
Lionnel7 <pierrerichan@gmail.com>
2025-04-11 13:03:13 +0200

    hhh

--- binary_trees.h
+++ binary_trees.h
@@ -1,13 +1,28 @@
-#include <stddef.h>
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stdio.h>
 #include <stdlib.h>
-typedef struct binary_tree_s
+
+/**
+ * struct binary_tree_s - Noeud d'arbre binaire
+ *
+ * @n: Entier stocké dans le nœud
+ * @parent: Pointeur vers le nœud parent
+ * @left: Pointeur vers le nœud enfant gauche
+ * @right: Pointeur vers le nœud enfant droit
+ */
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
 
+typedef struct binary_tree_s binary_tree_t;
+
+/* Prototypes de fonctions */
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -28,3 +43,5 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+#endif /* BINARY_TREES_H */
